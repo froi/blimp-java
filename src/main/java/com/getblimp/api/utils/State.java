edf3bf04eac76ed3941cc646d739f53a970e3e4b
Froilan Irizarry <irizarry.froilan@gmail.com>
2014-01-03 02:14:59 -0400

    Initial commit.
    Enum created to represent object state: Activie or Inactive

---
+++ src/main/java/com/getblimp/api/utils/State.java
@@ -0,0 +1,22 @@
+package com.getblimp.api.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: froilan
+ * Date: 14-1-2
+ * Time: 20:41
+ * To change this template use File | Settings | File Templates.
+ */
+public enum State {
+    INACTIVE("INACTIVE"),
+    ACTIVE("ACTIVE");
+
+    private String value;
+
+    State(String value){
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
+}
